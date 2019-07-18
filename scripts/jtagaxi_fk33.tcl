0c908c9614671f9853e62c42385af05ac4d27a73
d9 <D953i@users.noreply.github.com>
2019-07-18 17:46:15 -0400

    Create jtagaxi_fk33.tcl

---
+++ scripts/jtagaxi_fk33.tcl
@@ -0,0 +1,11 @@
+
+create_hw_axi_txn write0 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000000 -type write -force
+create_hw_axi_txn write1 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000001 -type write -force
+create_hw_axi_txn write2 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000002 -type write -force
+create_hw_axi_txn write3 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000003 -type write -force
+create_hw_axi_txn write4 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000004 -type write -force
+create_hw_axi_txn write5 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000005 -type write -force
+create_hw_axi_txn write6 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000006 -type write -force
+create_hw_axi_txn write7 [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x00000007 -type write -force
+
+run_hw_axi write1
