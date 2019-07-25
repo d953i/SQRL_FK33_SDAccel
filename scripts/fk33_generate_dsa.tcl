427125527f1bfee6de884927a926a2fb4bbb08f6
d9 <D953i@users.noreply.github.com>
2019-07-25 10:33:27 -0400

    Update to working verion for DSA creation script

--- scripts/fk33_generate_dsa.tcl
+++ scripts/fk33_generate_dsa.tcl
@@ -1,23 +1,35 @@
 
-set_property pfm.name fk33 [get_files ...]
-set_property dsa.vendor "xilinx" [current_project]
-set_property dsa.name "fk33" [current_project]
-set_property dsa.board_id "sqrl-fk33" [current_project]
+set_property pfm_name {squirrelsresearch.com:fk33:fk33platform:2019.1} [get_files {./fk33_sdaccel/fk33_sdaccel.srcs/sources_1/bd/bd/bd.bd}]
+set_property dsa.name {fk33platform} [current_project]
+set_property dsa.vendor "squirrelsresearch.com" [current_project]
+set_property dsa.board_id "fk33" [current_project]
+set_property dsa.version {2019.1} [current_project]
 
-set_property dsa.flash_interface_type “spix4” [current_project]
-set_property dsa.version "1.0" [current_project]
+#set_property pfm_name fk33platform [get_files ./fk33_sdaccel/fk33_sdaccel.srcs/sources_1/bd/bd/bd.bd}]
+#set_property dsa.vendor "sqrl" [current_project]
+#set_property dsa.name "fk33platform" [current_project]
+#set_property dsa.board_id "fk33" [current_project]
 
-set_property dsa.uses_pr true [current_project]
+#set_property dsa.flash_interface_type “spix4” [current_project]
+#set_property dsa.version "1.0" [current_project]
+
+set_property PFM.CLOCK {axi_aclk {id "1" is_default "true" proc_sys_reset "/xdma_reset"}} [get_bd_cells /xdma]
+set_property PFM.AXI_PORT {M02_AXI {memport "MIG" sptag "" memory ""} M03_AXI {memport "MIG" sptag "" memory ""} M04_AXI {memport "MIG" sptag "" memory ""} M05_AXI {memport "MIG" sptag "" memory ""} M06_AXI {memport "MIG" sptag "" memory ""} M07_AXI {memport "MIG" sptag "" memory ""}} [get_bd_cells /axi2pr]
+set_property PFM.AXI_PORT {S02_AXI {memport "MIG" sptag "" memory ""} S03_AXI {memport "MIG" sptag "" memory ""} S04_AXI {memport "MIG" sptag "" memory ""} S05_AXI {memport "MIG" sptag "" memory ""} S06_AXI {memport "MIG" sptag "" memory ""} S07_AXI {memport "MIG" sptag "" memory ""}} [get_bd_cells /pr2axi]
+
+#set_property dsa.uses_pr true [current_project]
 #set_property dsa.static_synth_checkpoint false [current_project]
 
-set_property dsa.pcie_id_vendor "0x10ee" [current_project]
-set_property dsa.pcie_id_device "0x8038" [current_project]
-set_property dsa.pcie_id_subsystem "0x0011" [current_project]
-set_property dsa.board_name "sqrl:fk33:a3" [current_project]
-set_property dsa.board_interface_type "gen3x16" [current_project]
-set_property dsa.board_memories {{hbm 8GB}} [current_project]
-set_property dsa.board_interface_name "PCIe" [current_project]
-set_property dsa.board_vendor "squirrelsresearch.com" [current_project]
-#set_property board_part "xcvu33p-fsvh2104-2-e" [current_project]
+#set_property dsa.pcie_id_vendor "0x10ee" [current_project]
+#set_property dsa.pcie_id_device "0x8038" [current_project]
+#set_property dsa.pcie_id_subsystem "0x0011" [current_project]
+#set_property dsa.board_name "sqrl:fk33" [current_project]
+#set_property dsa.board_interface_type "gen3x1" [current_project]
+#set_property dsa.board_memories {{hbm 4GB}} [current_project]
+#set_property dsa.board_interface_name "PCIe" [current_project]
+#set_property dsa.board_vendor "squirrelsresearch.com" [current_project]
+##set_property board_part "xcvu33p-fsvh2104-2-e" [current_project]
 
-write_dsa sqrl_fk33_2019_1.dsa -include_bit
+validate_bd_design -include_pfm -force
+
+write_dsa sqrl_fk33_xdma_2019_1.dsa -include_bit
--- scripts/fk33_sdaccel.tcl
+++ scripts/fk33_sdaccel.tcl
@@ -18,6 +18,10 @@ if {[file exists "$ProjectFolder"]} {
     file delete -force $ProjectFolder
 }
 
+set scriptPath [file dirname [file normalize [info script]]]
+#puts stdout $scriptPath
+#puts stdout [file dirname [info script]]
+#return -code 1
 
 create_project $ProjectName ./$ProjectName -part xcvu33p-fsvh2104-2-e
 
@@ -30,19 +34,28 @@ set_property -dict [list CONFIG.pl_link_cap_max_link_speed {8.0_GT/s} CONFIG.axi
 set_property -dict [list CONFIG.plltype {QPLL1}] [get_bd_cells xdma]
 set_property -dict [list CONFIG.pf0_device_id {9031} CONFIG.PF0_DEVICE_ID_mqdma {9031} CONFIG.PF2_DEVICE_ID_mqdma {9031} CONFIG.PF3_DEVICE_ID_mqdma {9031}] [get_bd_cells xdma]
 set_property -dict [list CONFIG.xdma_pcie_64bit_en {true} CONFIG.pf0_msix_cap_table_bir {BAR_1:0} CONFIG.pf0_msix_cap_pba_bir {BAR_1:0}] [get_bd_cells xdma]
+set_property -dict [list CONFIG.pcie_blk_locn {PCIE4C_X1Y0} CONFIG.axisten_freq {250} CONFIG.select_quad {GTY_Quad_227}] [get_bd_cells xdma]
+
+create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 xdma_reset
+connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins xdma_reset/slowest_sync_clk]
+connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins xdma_reset/ext_reset_in]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset:5.0 sys_reset
+connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins sys_reset/ext_reset_in]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:util_ds_buf:2.1 util_ds_buf_0
 set_property -dict [list CONFIG.C_BUF_TYPE {IBUFDSGTE}] [get_bd_cells util_ds_buf_0]
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:axi_apb_bridge:3.0 axi_apb_bridge_0
-set_property -dict [list CONFIG.C_APB_NUM_SLAVES {1}] [get_bd_cells axi_apb_bridge_0]
+#create_bd_cell -type ip -vlnv xilinx.com:ip:axi_apb_bridge:3.0 axi_apb_bridge_0
+#set_property -dict [list CONFIG.C_APB_NUM_SLAVES {1}] [get_bd_cells axi_apb_bridge_0]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:clk_wiz:6.0 clk_wiz_0
 set_property CONFIG.RESET_TYPE ACTIVE_LOW [get_bd_cells /clk_wiz_0]
+set_property -dict [list CONFIG.PRIM_IN_FREQ.VALUE_SRC USER] [get_bd_cells clk_wiz_0]
+set_property -dict [list CONFIG.PRIM_IN_FREQ {200}] [get_bd_cells clk_wiz_0]
+set_property -dict [list CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {100}] [get_bd_cells clk_wiz_0]
 
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins sys_reset/ext_reset_in]
+#connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins sys_reset/ext_reset_in]
 
 make_bd_intf_pins_external  [get_bd_intf_pins xdma/pcie_mgt]
 set_property name pcie [get_bd_intf_ports pcie_mgt_0]
@@ -59,7 +72,7 @@ set_property name pcie_resetn [get_bd_ports sys_rst_n_0]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect_0
 set_property name axi2pr [get_bd_cells axi_interconnect_0]
-set_property -dict [list CONFIG.NUM_SI {2} CONFIG.NUM_MI {3}] [get_bd_cells axi2pr]
+set_property -dict [list CONFIG.NUM_SI {2} CONFIG.NUM_MI {2}] [get_bd_cells axi2pr]
 
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_interconnect:2.1 axi_interconnect_1
 set_property name pr2axi [get_bd_cells axi_interconnect_1]
@@ -76,11 +89,12 @@ create_bd_cell -type ip -vlnv xilinx.com:ip:hbm:1.0 hbm
 set_property -dict [list CONFIG.USER_CLK_SEL_LIST0 {AXI_00_ACLK} CONFIG.USER_MC0_EN_DATA_MASK {true} CONFIG.USER_MC1_EN_DATA_MASK {true} CONFIG.USER_MC2_EN_DATA_MASK {true} CONFIG.USER_MC3_EN_DATA_MASK {true} CONFIG.USER_MC4_EN_DATA_MASK {true} CONFIG.USER_MC5_EN_DATA_MASK {true} CONFIG.USER_MC6_EN_DATA_MASK {true} CONFIG.USER_MC7_EN_DATA_MASK {true} CONFIG.USER_MC8_EN_DATA_MASK {true} CONFIG.USER_MC9_EN_DATA_MASK {true} CONFIG.USER_MC10_EN_DATA_MASK {true} CONFIG.USER_MC11_EN_DATA_MASK {true} CONFIG.USER_MC12_EN_DATA_MASK {true} CONFIG.USER_MC13_EN_DATA_MASK {true} CONFIG.USER_MC14_EN_DATA_MASK {true} CONFIG.USER_MC15_EN_DATA_MASK {true} CONFIG.USER_SAXI_01 {false} CONFIG.USER_SAXI_02 {false} CONFIG.USER_SAXI_03 {false} CONFIG.USER_SAXI_04 {false} CONFIG.USER_SAXI_05 {false} CONFIG.USER_SAXI_06 {false} CONFIG.USER_SAXI_07 {false} CONFIG.USER_SAXI_08 {false} CONFIG.USER_SAXI_09 {false} CONFIG.USER_SAXI_10 {false} CONFIG.USER_SAXI_11 {false} CONFIG.USER_SAXI_12 {false} CONFIG.USER_SAXI_13 {false} CONFIG.USER_SAXI_14 {false} CONFIG.USER_SAXI_15 {false}] [get_bd_cells hbm]
 set_property -dict [list CONFIG.USER_HBM_REF_CLK_0 {200}] [get_bd_cells hbm]
 set_property -dict [list CONFIG.USER_AXI_CLK_FREQ {250} CONFIG.USER_AXI_INPUT_CLK_FREQ {250} CONFIG.USER_AXI_INPUT_CLK_NS {4.000} CONFIG.USER_AXI_INPUT_CLK_PS {4000} CONFIG.USER_AXI_INPUT_CLK_XDC {4.000} CONFIG.HBM_MMCM_FBOUT_MULT0 {80}] [get_bd_cells hbm]
+set_property CONFIG.USER_APB_EN false [get_bd_cells /hbm]
 #make_bd_pins_external  [get_bd_pins hbm/HBM_REF_CLK_0]
 #set_property name hbm_ref [get_bd_ports HBM_REF_CLK_0_0]
 connect_bd_intf_net [get_bd_intf_pins hbm/SAXI_00] -boundary_type upper [get_bd_intf_pins pr2axi/M00_AXI]
-connect_bd_intf_net -boundary_type upper [get_bd_intf_pins axi2pr/M02_AXI] [get_bd_intf_pins axi_apb_bridge_0/AXI4_LITE]
-connect_bd_intf_net [get_bd_intf_pins axi_apb_bridge_0/APB_M] [get_bd_intf_pins hbm/SAPB_0]
+#connect_bd_intf_net -boundary_type upper [get_bd_intf_pins axi2pr/M02_AXI] [get_bd_intf_pins axi_apb_bridge_0/AXI4_LITE]
+#connect_bd_intf_net [get_bd_intf_pins axi_apb_bridge_0/APB_M] [get_bd_intf_pins hbm/SAPB_0]
 set_property -dict [list CONFIG.FREQ_HZ {200000000}] [get_bd_pins hbm/HBM_REF_CLK_0]
                                                                                                       
 create_bd_cell -type ip -vlnv xilinx.com:ip:ocl_block:1.0 ocl_block
@@ -107,36 +121,33 @@ connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins axi2pr/S00_ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins axi2pr/M00_ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins axi2pr/M01_ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins axi2pr/S01_ACLK]
+
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins pr2axi/ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins pr2axi/S00_ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins pr2axi/M00_ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins pr2axi/S01_ACLK]
-connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins clk_wiz_0/clk_in1]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins jtag_axi/aclk]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins ocl_block/ACLK]
 connect_bd_net [get_bd_pins xdma/axi_aclk] [get_bd_pins hbm/AXI_00_ACLK]
 
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins jtag_axi/aresetn]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins axi2pr/ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins axi2pr/S00_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins axi2pr/M00_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins axi2pr/M01_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins axi2pr/S01_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins ocl_block/ARESET]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins pr2axi/ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins pr2axi/S00_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins pr2axi/M00_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins pr2axi/S01_ARESETN]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins hbm/AXI_00_ARESET_N]
-connect_bd_net [get_bd_pins xdma/axi_aresetn] [get_bd_pins clk_wiz_0/resetn]
+connect_bd_net [get_bd_pins xdma_reset/interconnect_aresetn] [get_bd_pins axi2pr/ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/interconnect_aresetn] [get_bd_pins pr2axi/ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins jtag_axi/aresetn]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins clk_wiz_0/resetn]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins axi2pr/S00_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins axi2pr/M00_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins axi2pr/M01_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins axi2pr/S01_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins pr2axi/S00_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins pr2axi/M00_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins pr2axi/S01_ARESETN]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins hbm/AXI_00_ARESET_N]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins ocl_block/ARESET]
 
-connect_bd_net [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins axi2pr/M02_ACLK]
-connect_bd_net [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins axi_apb_bridge_0/s_axi_aclk]
+connect_bd_net [get_bd_pins util_ds_buf_1/IBUF_OUT] [get_bd_pins clk_wiz_0/clk_in1]
 connect_bd_net [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins hbm/APB_0_PCLK]
 connect_bd_net [get_bd_pins clk_wiz_0/clk_out1] [get_bd_pins sys_reset/slowest_sync_clk]
 connect_bd_net [get_bd_pins clk_wiz_0/locked] [get_bd_pins sys_reset/dcm_locked]
-connect_bd_net [get_bd_pins sys_reset/peripheral_aresetn] [get_bd_pins axi2pr/M02_ARESETN]
-connect_bd_net [get_bd_pins sys_reset/peripheral_aresetn] [get_bd_pins axi_apb_bridge_0/s_axi_aresetn]
 connect_bd_net [get_bd_pins sys_reset/peripheral_aresetn] [get_bd_pins hbm/APB_0_PRESET_N]
 
 set_property CONFIG.ENABLE_ADVANCED_OPTIONS 1 [get_bd_cells ocl_block]
@@ -148,10 +159,10 @@ set_property -dict [list CONFIG.NUM_MI {2}] [get_bd_cells pr2axi]
 create_bd_cell -type ip -vlnv xilinx.com:ip:axi_gpio:2.0 axi_gpio_0
 connect_bd_intf_net [get_bd_intf_pins axi_gpio_0/S_AXI] -boundary_type upper [get_bd_intf_pins pr2axi/M01_AXI]
 connect_bd_net [get_bd_pins axi_gpio_0/s_axi_aclk] [get_bd_pins xdma/axi_aclk]
-connect_bd_net [get_bd_pins axi_gpio_0/s_axi_aresetn] [get_bd_pins xdma/axi_aresetn]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins axi_gpio_0/s_axi_aresetn]
 connect_bd_net [get_bd_pins pr2axi/M01_ACLK] [get_bd_pins xdma/axi_aclk]
-connect_bd_net [get_bd_pins pr2axi/M01_ARESETN] [get_bd_pins xdma/axi_aresetn]
-set_property -dict [list CONFIG.C_GPIO_WIDTH {7} CONFIG.C_ALL_OUTPUTS {1}] [get_bd_cells axi_gpio_0]
+connect_bd_net [get_bd_pins xdma_reset/peripheral_aresetn] [get_bd_pins pr2axi/M01_ARESETN]
+set_property -dict [list CONFIG.C_GPIO_WIDTH {6} CONFIG.C_ALL_OUTPUTS {1}] [get_bd_cells axi_gpio_0]
 make_bd_intf_pins_external  [get_bd_intf_pins axi_gpio_0/GPIO]
 set_property name led [get_bd_intf_ports GPIO_0]
 
@@ -165,15 +176,10 @@ save_bd_design
 
 #set_property PR_FLOW 1 [current_project]
 
+add_files -fileset constrs_1 -norecurse $scriptPath/fk33_sdaccel.xdc
+import_files -fileset constrs_1 $scriptPath/fk33_sdaccel.xdc
+
 make_wrapper -files [get_files ./$ProjectName/$ProjectName.srcs/sources_1/bd/bd/bd.bd] -top
 add_files -norecurse ./$ProjectName/$ProjectName.srcs/sources_1/bd/bd/hdl/bd_wrapper.v
 update_compile_order -fileset sources_1
-
-#add_files -fileset constrs_1 -norecurse ./ZCU104_SODIMM.xdc
-#import_files -fileset constrs_1 ./ZCU104_SODIMM.xdc
-
-#make_wrapper -files [get_files ./$ProjectName/$ProjectName.srcs/sources_1/bd/design_1/design_1.bd] -top
-#add_files -norecurse ./$ProjectName/$ProjectName.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
-#update_compile_order -fileset sources_1
-
 
