e9c7f5ab3b1a3e514c567bc980165490c9ea14c0
d9 <d9@idv-tech.com>
2019-08-06 08:14:31 -0400

    Update fk33_jtagaxi.tcl

--- scripts/fk33_jtagaxi.tcl
+++ scripts/fk33_jtagaxi.tcl
@@ -1,10 +1,11 @@
-create_hw_axi_txn write0 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000000 -type write -force
-create_hw_axi_txn write1 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000001 -type write -force
-create_hw_axi_txn write2 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000002 -type write -force
-create_hw_axi_txn write3 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000003 -type write -force
-create_hw_axi_txn write4 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000004 -type write -force
-create_hw_axi_txn write5 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000005 -type write -force
-create_hw_axi_txn write6 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000006 -type write -force
-create_hw_axi_txn write7 [get_hw_axis hw_axi_1] -address 0x100010000 -data 0x00000007 -type write -force
+run_hw_axi [create_hw_axi_txn Jtag2AXI [get_hw_axis hw_axi_1] -address 0x40000000 -data 0x0000003F -type write -force]
 
-run_hw_axi write1
+
+##run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x200010000 -data 0x0000000000000044 -type write -size 64 -force]
+
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800100 -data 0x00000003 -type write -force]
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800108 -data 0x0000002C -type write -force]
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800108 -data 0x00000044 -type write -force]
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800100 -data 0x0000000D -type write -force]
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800108 -data 0x00000044 -type write -force]
+run_hw_axi [create_hw_axi_txn setVCCINT [get_hw_axis hw_axi_1] -address 0x40800100 -data 0x00000009 -type write -force]
